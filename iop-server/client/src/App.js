4d6623d0bfb1d53701ddb37952a7f7eab35e9e85
Nick Sweet <nckswt@gmail.com>
2018-06-03 21:15:04 -0500

    more styling

--- iop-server/client/src/App.js
+++ iop-server/client/src/App.js
@@ -6,8 +6,13 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 import './App.css';
 
 const theme = createMuiTheme({
@@ -28,6 +33,14 @@ const theme = createMuiTheme({
 });
 
 const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  deviceRow: {
+    padding: theme.spacing.unit * 1,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
   card: {
     minWidth: 275,
     maxWidth: 800,
@@ -36,11 +49,13 @@ const styles = {
     marginRight : 'auto'
   },
   title: {
-    marginBottom: 16,
-    fontSize: 14,
+    marginBottom: 8,
+    textAlign: 'left'
   },
-  pos: {
-    marginBottom: 12,
+  deviceInfoTable: {
+    marginTop: 0,
+    paddingTop: 0
+
   },
   online: {
     float: 'right',
@@ -48,7 +63,7 @@ const styles = {
   },
   offline: {
     float: 'right',
-  }
+  },
 };
 
 
@@ -60,6 +75,76 @@ function OnlineChip(props) {
   return <Chip className={props.classes.offline} label="Offline"/>
 }
 
+function DeviceInfo(props) {
+  var device = props.device;
+  var classes = props.classes;
+  var date = new Date();
+  return (
+    <Table className={classes.deviceInfoTable}>
+      <TableBody>
+        <TableRow key={device.id}>
+          <TableCell component="th" scope="row">
+          id
+          </TableCell>
+          <TableCell numeric>{device.id}</TableCell>
+        </TableRow>
+        <TableRow key={device.id}>
+          <TableCell component="th" scope="row">
+          Last Update
+          </TableCell>
+          <TableCell numeric>
+          {device.last_update}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
+}
+
+function DeviceGraph(props) {
+  var device = props.device;
+  var classes = props.classes;
+  return (
+    <Typography component="p">
+      DATA GRAPH
+    </Typography>
+  )
+}
+
+function DeviceCard(props) {
+  var device = props.device;
+  var classes = props.classes;
+
+  return (
+  <div key={device.id} id={"device" + device.id}>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} variant="headline" component="h2">
+          {device.nicename}
+          <OnlineChip device={device} classes={classes}/>
+        </Typography>
+
+        <Grid container className={classes.deviceRow} justify="center" spacing={20}>
+          <Grid item xs={12} sm={3}>
+
+            <DeviceInfo device={device} classes={classes}/>
+          </Grid>
+
+          <Grid item xs={12} sm={9}>
+
+            <DeviceGraph device={device} classes={classes}/>
+          </Grid>
+
+        </Grid>
+
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary" style={{flex: 4}} >View Raw Data</Button>
+      </CardActions>
+    </Card>
+  </div>
+  )
+}
 
 class App extends Component {
   state = {devices: []}
@@ -71,35 +156,20 @@ class App extends Component {
    }
 
    render() {
-     console.log(this.props.classes);
+     var classes = this.props.classes;
      return (
        <div className="App">
          <CssBaseline />
          <MuiThemeProvider theme={theme}>
 
          <h1>Devices</h1>
-         {this.state.devices.map(device =>
-           <div key={device.id} id={"device" + device.id}>
-             <Card className={this.props.classes.card}>
-               <CardContent>
-                 <Typography variant="headline" component="h2">
-                   {device.nicename}
-                   <OnlineChip device={device} classes={this.props.classes}/>
-                 </Typography>
-
-                 <Typography className={this.props.classes.deviceProperty} color="textSecondary">
-                   id: {device.id}
-                 </Typography>
-                 <Typography component="p">
-                   DATA GRAPH
-                 </Typography>
-               </CardContent>
-               <CardActions>
-                 <Button size="small" color="primary" >View Raw Data</Button>
-               </CardActions>
-             </Card>
-           </div>
-       )}
+           <Grid container className={classes.deviceRow} justify="center" spacing={20}>
+             {this.state.devices.map(device =>
+              <Grid item xs={12}>
+              <DeviceCard  device={device} classes={classes}/>
+              </Grid>
+           )}
+        </Grid>
        </MuiThemeProvider>
      </div>
      );
--- iop-server/routes/devices.js
+++ iop-server/routes/devices.js
@@ -3,9 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    // For todays date;
+  Date.prototype.today = function () {
+      return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+  }
+
+  // For the time now
+  Date.prototype.timeNow = function () {
+       return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+  }
+  now = "" + new Date().today() + " " + new Date().timeNow();
+  console.log(now);
   res.json([
-    {id:1, nicename:"Spider Plant Monitor", online: true},
-    {id:2, nicename:"English Ivy Monitor", online: false}
+    {id:1, last_update: now, nicename:"Spider Plant Monitor", online: true},
+    {id:2, last_update: now, nicename:"English Ivy Monitor", online: false}
   ]);
 });
 
